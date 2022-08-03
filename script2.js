0d40138184e201666d84953a3df1f3dd1eb99643
Noah <nleyja@gmail.com>
2022-08-03 17:32:41 -0500

    added operator functionality

--- script2.js
+++ script2.js
@@ -2,24 +2,44 @@ const numButtons = document.querySelectorAll(".num-button");
 const operButtons = document.querySelectorAll(".oper-button");
 const equalButton = document.querySelector(".equal-button");
 const outputDisplay = document.querySelector(".screen-output")
+const clearButton = document.querySelector(".clear-button");
 
-const handleNumbers = (event) => {
+const handleAllButtons = (event) => {
   outputDisplay.innerHTML += event.target.innerHTML;
 };
 
 for (let i = 0; i < numButtons.length; i++) {
-  numButtons[i].addEventListener("click", handleNumbers);
+  numButtons[i].addEventListener("click", handleAllButtons);
 };
 
-const handleOperators = () => {
-outputDisplay.innerHTML = operButtons.innerHTML;
-
+for (let i = 0; i < operButtons.length; i++) {
+  operButtons[i].addEventListener("click", handleAllButtons);
 };
 
-const handleEqualButton = () => {
-// equal button will equate values in output display
+// const handleClearButton = () => {
+//   outputDisplay.innerHTML = " ";
+// };
+
+// clearButton.addEventListener("click", handleClearButton);
+
+// const handleEqualButton = () => {
+//   outputDisplay.innerHTML = 
+// };
+
+// equalButton.addEventListener("click", handleEqualButton);
+
+
+
+
+
+
+
+
+
+// const handleEqualButton = () => {
+// // equal button will equate values in output display
   
-};
+// };
 
-operButtons.addEventListener("click", handleOperators);
-equalButton.addEventListener("click", handleEqualButton);
+// operButtons.addEventListener("click", handleAllButtons);
+// equalButton.addEventListener("click", handleEqualButton);
