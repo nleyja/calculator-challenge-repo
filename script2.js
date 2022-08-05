f232f368a1c30efd635fd1c8d610a590455008af
Noah <nleyja@gmail.com>
2022-08-05 10:20:43 -0500

    finally was able to seperate first number to second numbergit status

--- script2.js
+++ script2.js
@@ -1,20 +1,42 @@
 const numButtons = document.querySelectorAll(".num-button");
 const operButtons = document.querySelectorAll(".oper-button");
 const equalButton = document.querySelector(".equal-button");
-const outputDisplay = document.querySelector(".screen-output")
+const outputDisplay = document.querySelector(".screen-output");
 const clearButton = document.querySelector(".clear-button");
 
-const handleAllButtons = (event) => {
+let fistInputNumber;
+let secondInputNumber;
+let operatorSign;
+let calculateButton
+
+const handleNumButtons = (event) => {
   outputDisplay.innerHTML += event.target.innerHTML;
+  
 };
 
 for (let i = 0; i < numButtons.length; i++) {
-  numButtons[i].addEventListener("click", handleAllButtons);
+  numButtons[i].addEventListener("click", handleNumButtons);
+};
+
+const handleOperButtons = (event) => {
+  outputDisplay.innerHTML += event.target.innerHTML;
+  fistInputNumber = outputDisplay.innerHTML.slice(0, -1);
+  operatorSign = outputDisplay.innerHTML.slice(-1);
+  outputDisplay.innerHTML = " "
 };
 
 for (let i = 0; i < operButtons.length; i++) {
-  operButtons[i].addEventListener("click", handleAllButtons);
+  operButtons[i].addEventListener("click", handleOperButtons);
 };
+
+
+const handleCalculateButton = (event) => {
+  outputDisplay.innerHTML += event.target.innerHTML;
+  secondInputNumber = outputDisplay.innerHTML.slice(0, -1)
+  calculateButton = outputDisplay.innerHTML.slice(-1);
+};
+
+equalButton.addEventListener("click", handleCalculateButton);
 
 const handleClearButton = () => {
   outputDisplay.innerHTML = " ";
@@ -22,18 +44,12 @@ const handleClearButton = () => {
 
 clearButton.addEventListener("click", handleClearButton);
 
-// const handleEqualButton = () => {
-//   outputDisplay.innerHTML = 
-// };
-
-// equalButton.addEventListener("click", handleEqualButton);
 
 
 
-
-
-
-
+//  if(operatorSign == "+") {
+  //   result = fistInputNumber + secondInputNumber;
+  //  }
 
 
 // const handleEqualButton = () => {
